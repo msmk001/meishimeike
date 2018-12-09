1f08f3ca75611963bfcc051cb966eefc69f87a0c
zhou151 <1045907858@qq.com>
2018-12-09 18:05:18 +0800

    -

--- meishimeike/src/test/java/com/zhou/test/MerchantTest.java
+++ meishimeike/src/test/java/com/zhou/test/MerchantTest.java
@@ -2,11 +2,18 @@ package com.zhou.test;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.zhou.meishimeike.dao.MerchantDao;
 import com.zhou.meishimeike.entity.Merchant;
 
 public class MerchantTest {
+	
+	private static final Logger logger = LogManager.getLogger(MerchantTest.class);
+	
 	public static void main(String[] args) {
+		logger.info("ok");
 		test05();
 	}
 	public static void test03() {
@@ -24,5 +31,7 @@ public class MerchantTest {
 		
 		Merchant merchantById = testMapper.getMerchantById(7);
 		System.out.println(": "+merchantById.getClassifyList());
+		
+		
 	}
 }
